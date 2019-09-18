3cce3ddcb5530d403e1c45bc1b61dbd6c82e205f
Marco <inspiredtolive0@gmail.com>
2019-09-18 04:27:06 +0000

    Recursively checks for palindrome lists

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,31 @@
 #include "lists.h"
 
 /**
+ * recurse - recursively checks if a linked list is a palindrome
+ * @start: node to compare to end
+ * @end: node to compare to start
+ * Return: Palindrome(1) or Not(0)
+ */
+int recurse(listint_t **start, listint_t *end)
+{
+	int result;
+
+	if (!end)
+		return (1);
+	result = recurse(start, end->next) && ((*start)->n == end->n);
+	*start = (*start)->next;
+	return result;
+}
+
+/**
  * is_palindrome - Checks if a linked list is a palindrome
  * @head: pointer to the head of a linked list
  * Return: Palindrome(1) or Not(0)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *node = *head, *rev, *tmp;
+	listint_t *node = *head;
 
-	if (!node)
-		return (1);
-	for (rev = NULL; node; node = node->next)
-	{
-		tmp = malloc(sizeof(*tmp));
-		tmp->n = node->n;
-		tmp->next = rev;
-		rev = tmp;
-	}
-	for (node = *head, tmp = rev; node; node = node->next, rev = rev->next)
-	{
-		if (node->n != rev->n)
-		{
-			free(tmp);
-			return (0);
-		}
-	}
-	return (1);
-
+	return (recurse(&node, node));
 }
 
