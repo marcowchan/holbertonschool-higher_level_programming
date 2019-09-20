efcd147f6a6e0ec8aece6b967ca031b7a3462eb9
Marco <inspiredtolive0@gmail.com>
2019-09-20 05:22:23 +0000

    Convert roman numerals into integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    prev = 0
+    integer = 0
+    for c in roman_string:
+        if num[c] > prev:
+            integer += num[c] - prev - prev
+            prev = num[c]
+        else:
+            integer += num[c]
+            prev = num[c]
+    return integer
