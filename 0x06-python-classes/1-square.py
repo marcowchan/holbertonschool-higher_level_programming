602588918b3c1008104e9057bb65809693a76d30
Marco Chan <inspiredtolive0@gmail.com>
2019-10-01 20:33:58 -0700

    Creates a class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a 'Square' class with a private instance attribute"""
+
+
+class Square:
+    """Empty definition of 'Square'"""
+    def __init__(self, size=0):
+        """Instantiates a private instance attribute 'size'"""
+        self.__size = size
