6ec83515f66d64e30f220fc129548a3bc8944a3c
Marco Chan <inspiredtolive0@gmail.com>
2019-10-03 15:31:27 -0700

    Prints a square with '#'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a print_square function."""
+
+
+def print_square(size):
+    """Prints a square with '#'.
+
+    Args:
+        size: The size of the square.
+    Raises:
+        TypeError: If the size is not an integer
+        ValueError: The size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print("\n".join(["#" * size] * size))
