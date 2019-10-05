f3869e98b785bc53483a122ffc4c7999e5598c24
Marco Chan <inspiredtolive0@gmail.com>
2019-10-04 20:47:04 -0700

    Strips spaces of each printed line

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a text_indentation function."""
+
+
+def text_indentation(text):
+    """Prints a text with 2 newlines after: '.', '?' and ':'.
+
+    Args:
+        text: The text to print.
+    Raises:
+        TypeError: If the text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    special = {".", "?", ":"};
+    begin = 0
+    for end in range(len(text)):
+        if text[end] in special:
+            line = text[begin:end + 1] + "\n\n"
+            begin = end + 1
+            print("{}".format(line.strip(" ")), end="")
+    if begin <= end:
+        line = text[begin:end + 1]
+        print("{}".format(line.strip(" ")), end="")
