7f0b6c9569a8964a05aa2a94f782144fd4c04bbc
Marco Chan <inspiredtolive0@gmail.com>
2019-10-07 17:40:44 -0700

    Converts bigger_or_equal to a static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -90,6 +90,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Computes the larger Rectangle based on area."""
         if not isinstance(rect_1, Rectangle):
