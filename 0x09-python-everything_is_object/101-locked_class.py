3c105201c82fab431aec1fda9b2187086cddbe51
Marco Chan <inspiredtolive0@gmail.com>
2019-10-08 17:48:17 -0700

    Creates a class that prevents dynamic creation of instance attributes.

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a LockedClass class."""
+
+
+class LockedClass:
+    """A class with no attributes."""
+    def __setattr__(self, name, value):
+        """Only allows the dynamic creation of the 'first_name' attribute.
+
+        Raises:
+            AttributeError: If name is not 'first_name'.
+        """
+        if name == "first_name":
+            object.__setattr__(self, 'first_name', name)
+        else:
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name))
