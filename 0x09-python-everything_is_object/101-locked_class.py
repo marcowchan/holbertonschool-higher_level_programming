9eb34ff8279c745e01d00f7c1cef33d9cdc08282
Marco Chan <inspiredtolive0@gmail.com>
2019-10-08 18:04:39 -0700

    Removes all docstrings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-"""Defines a LockedClass class."""
-
-
 class LockedClass:
     def __setattr__(self, name, value):
-        """Only allows the dynamic creation of the 'first_name' attribute.
-
-        Raises:
-            AttributeError: If name is not 'first_name'.
-        """
         if name == "first_name":
             object.__setattr__(self, 'first_name', value)
         else:
