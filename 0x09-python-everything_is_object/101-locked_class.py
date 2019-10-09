ff6ba818c97c136d17c8c6b09c1c016524f7cd77
Marco Chan <inspiredtolive0@gmail.com>
2019-10-08 18:09:07 -0700

    Use slots for a smaller memory footprint

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            object.__setattr__(self, 'first_name', value)
-        else:
-            raise AttributeError(
-                "'LockedClass' object has no attribute '{}'".format(name))
+    __slots__ = ["first_name"]
