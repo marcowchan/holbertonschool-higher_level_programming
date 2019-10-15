143e418946bb5cdd3a1ae5b7b5c65c55586805e4
Marco Chan <inspiredtolive0@gmail.com>
2019-10-15 14:41:22 -0700

    Validates an integer value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,3 +7,19 @@ class BaseGeometry:
     def area(self):
         """Unimplemented area method."""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates a value.
+
+        Args:
+            name: A string name of the value.
+            value: An integer value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
