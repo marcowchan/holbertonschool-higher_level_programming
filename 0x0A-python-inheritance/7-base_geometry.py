a4073131a4a5da2b463ea0ad54a222b7d4cdaaa5
Marco Chan <inspiredtolive0@gmail.com>
2019-10-15 14:05:10 -0700

    Creates unimplemented area method.

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class with an empty definition."""
+    def area(self):
+        """Unimplemented area method."""
+        raise Exception("area() is not implemented")
