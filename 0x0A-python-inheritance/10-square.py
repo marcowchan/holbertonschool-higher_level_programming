e1bcd9c4bfd2b1895c40ebe88d23d06448a7ebc1
Marco Chan <inspiredtolive0@gmail.com>
2019-10-16 12:50:06 -0700

    Validates with size name

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,4 +7,5 @@ class Square(Rectangle):
     """A class that inherits from Rectangle."""
     def __init__(self, size):
         """Initalizes width and height to size."""
+        self.integer_validator("size", size)
         super().__init__(size, size)
