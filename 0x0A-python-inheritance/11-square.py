709193a77bd19cec175ecce345b2bb9b31b67591
Marco Chan <inspiredtolive0@gmail.com>
2019-10-15 20:52:06 -0700

    Changes the string representation of the Square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that inherits from Rectangle."""
+    def __init__(self, size):
+        """Initalizes width and height to size."""
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns string representation of the Square."""
+        return "[Square] {:d}/{:d}".format(self.__width, self.__height)
