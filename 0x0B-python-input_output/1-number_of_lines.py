d99b69edb5259c4e0360edeb847b2b79283482f9
Marco Chan <inspiredtolive0@gmail.com>
2019-10-16 20:26:07 -0700

    Computes the number of lines in a file

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a number_of_lines() function."""
+
+
+def number_of_lines(filename=""):
+    """Computes the number of lines in a file.
+
+    Args:
+        filename: The name and path of the file to print.
+    """
+    lines = 0
+    with open(filename, "r", encoding="utf-8") as f:
+        while f.readline() != "":
+            lines += 1
+    return lines
