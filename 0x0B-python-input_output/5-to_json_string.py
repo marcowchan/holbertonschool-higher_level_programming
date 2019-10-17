d32213c42ac4f7b4266837ea2e1518154cfb7072
Marco Chan <inspiredtolive0@gmail.com>
2019-10-16 21:08:13 -0700

    Gets the JSON representation of an object

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a to_json_string() function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Gets the JSON representation of an object.
+
+    Args:
+        my_obj: The object to serialize.
+
+    Returns:
+        The JSON string of the object.
+    """
+    return json.dumps(my_obj)
