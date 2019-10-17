5894031934d34b3c4af5cb7606610aff388f29a0
Marco Chan <inspiredtolive0@gmail.com>
2019-10-16 20:49:33 -0700

    Writes a string to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a write_file() function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a file.
+
+    Args:
+        filename: Path and name of the file.
+        text: The string to write.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+    return 0
