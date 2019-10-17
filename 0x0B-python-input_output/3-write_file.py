02c1550f13fe25d1267684876f70542796f3e88b
Marco Chan <inspiredtolive0@gmail.com>
2019-10-16 20:57:14 -0700

    Adds return section to the docstring

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -8,6 +8,8 @@ def write_file(filename="", text=""):
     Args:
         filename: Path and name of the file.
         text: The string to write.
+    Returns:
+        The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
