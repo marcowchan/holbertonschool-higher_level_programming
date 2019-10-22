617c1a023c8d4ed69ccf07049a09de7b74a1226a
Marco Chan <inspiredtolive0@gmail.com>
2019-10-21 17:57:17 -0700

    Creates unittests for the Base class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Unittest for the Base class."""
+import unittest
+from models.base import Base
+
+
+class BaseTest(unittest.TestCase):
+    """Tests for the Base class."""
+    def setUp(self):
+        """Initializes the __nb_objects attribute before each test."""
+        Base._Base__nb_objects = 0
+
+    def test_id_type(self):
+        """Checks if the id is an integer type."""
+        my_class = Base()
+        self.assertIs(type(my_class.id), int)
+
+    def test_id_increments(self):
+        """Checks if the ids increment."""
+        for i in range(1, 6):
+            with self.subTest(i=i):
+                my_class = Base()
+                self.assertEqual(my_class.id, i)
+
+    def test_sets_id(self):
+        """Checks if the id can be set."""
+        my_class = Base(13)
+        self.assertEqual(my_class.id, 13)
+
+    def test_sets_id_increments(self):
+        """Checks if the ids increment after setting an id."""
+        for i in range(1, 10):
+            with self.subTest(i=i):
+                if i == 5:
+                    my_class = Base(13)
+                    self.assertEqual(my_class.id, 13)
+                else:
+                    my_class = Base()
+                    if i < 5:
+                        self.assertEqual(my_class.id, i)
+                    else:
+                        self.assertEqual(my_class.id, i - 1)
+
+    def test_sets_negative_id(self):
+        """Checks if the id can be set with a negative integer."""
+        my_class = Base(-7)
+        self.assertEqual(my_class.id, -7)
