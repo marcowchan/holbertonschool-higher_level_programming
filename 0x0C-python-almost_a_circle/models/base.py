8f228a16dd74087efe0a5eea5fa48c8aa68acc05
Marco Chan <inspiredtolive0@gmail.com>
2019-10-21 17:57:37 -0700

    Creates a Base class with incrementing ids

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines an __init__() function."""
+
+
+class Base:
+    """A class with a counter attribute used to identify instances."""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Sets the id of the instance.
+
+        Args:
+            id: The ID of the instance.
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
