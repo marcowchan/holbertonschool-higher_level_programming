6cc04b0f9b3e32a5d25eb6027734e3cb5005a9b0
Marco Chan <inspiredtolive0@gmail.com>
2019-10-22 21:38:57 -0700

    Creates setter and getter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initalizes attributes."""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Returns the string representation of the Square instance."""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.width
         )
+
+    @property
+    def size(self):
+        """Gets the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size attribute."""
+        self.width = value
+        self.height = value
+        self.__size = value
