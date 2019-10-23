58655fab9d7a23dde01c4bd381e403a034f03a88
Marco Chan <inspiredtolive0@gmail.com>
2019-10-22 19:53:35 -0700

    Updates the display method to account for the position

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,9 +75,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints a rectangle of '#'."""
-        print("\n".join(["#" * self.width] * self.height))
+        print("\n" * self.y, end="")
+        print("\n".join([" " * self.x + "#" * self.width] * self.height))
 
     def __str__(self):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height
+            self.id, self.x, self.y, self.width, self.height
         )
