576a67085f64a3a9562f854b1cce8a42927c3a72
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 15:21:04 -0800

    Lists all privileges of MySQL users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of MySQL users.
+-- Lists privileges of user_0d_1.
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+-- Lists privileges of user_0d_2.
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
