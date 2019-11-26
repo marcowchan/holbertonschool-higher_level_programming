250168a6b5b90b675a80ad2d169de96b5a2ba11e
Marco Chan <inspiredtolive0@gmail.com>
2019-11-25 20:18:00 -0800

    Creates a table and adds multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creates a table and adds multiple rows.
+-- Creates a table.
+CREATE TABLE IF NOT EXISTS second_table ( id INT, name VARCHAR(256), score INT);
+-- Inserts multiple rows.
+INSERT INTO
+	second_table (id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
