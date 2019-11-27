2623db0f7b0cf7332382da0bea21af97b06c60b3
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 16:25:32 -0800

    Changes order of columns

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,8 +6,8 @@ USE `hbtn_0d_usa`;
 -- Creates a cities table.
 CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT AUTO_INCREMENT NOT NULL,
-	`name` VARCHAR(256) NOT NULL,
 	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`id`),
 	FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
 );
