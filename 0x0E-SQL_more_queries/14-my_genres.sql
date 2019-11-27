c3d8c8d6d6256ecf9d001570c4cebd88f4d7c66b
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 19:24:54 -0800

    Places where conditions on separate lines

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,7 @@
 -- Lists all genres of the show Dexter.
 SELECT tv_genres.name
 	FROM tv_genres, tv_show_genres, tv_shows
-	WHERE tv_shows.title = "Dexter" AND tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+	AND tv_show_genres.show_id = tv_shows.id
 	AND tv_show_genres.genre_id = tv_genres.id
 	ORDER BY name;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,7 @@
 -- Lists all comedy shows.
 SELECT tv_shows.title
 	FROM tv_genres, tv_show_genres, tv_shows
-	WHERE tv_genres.name = "Comedy" AND tv_show_genres.show_id = tv_shows.id
+	WHERE tv_genres.name = "Comedy"
+	AND tv_show_genres.show_id = tv_shows.id
 	AND tv_show_genres.genre_id = tv_genres.id
 	ORDER BY title;
