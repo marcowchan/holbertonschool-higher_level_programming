883c6b155d5e9f68e2bab78d8f423c7b1d3f2542
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 16:07:45 -0800

    Creates a hbtn_0d_usa database with a the states table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates a hbtn_0d_usa database with a the states table.
+-- Creates a hbtn_0d_usa database.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- Use the hbtn_0d_usa database.
+USE `hbtn_0d_usa`;
+-- Creates a cities table.
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	`name` VARCHAR(256) NOT NULL,
+	`state_id` INT NOT NULL,
+	FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
+);
