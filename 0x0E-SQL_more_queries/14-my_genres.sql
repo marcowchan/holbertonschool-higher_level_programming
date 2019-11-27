a1b035800178e8c0fcf9c61c7ce6ebdb29330bd1
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 18:18:44 -0800

    Capitalizes AS

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Lists all genres of the show Dexter.
 -- Lists all genres of the show Dexter.
-SELECT tv_genres.name as name
+SELECT tv_genres.name AS name
 	FROM tv_genres, tv_show_genres, tv_shows
 	WHERE tv_shows.title = "Dexter" AND tv_show_genres.show_id = tv_shows.id
 	AND tv_show_genres.genre_id = tv_genres.id
