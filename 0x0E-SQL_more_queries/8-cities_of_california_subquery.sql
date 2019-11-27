262a199e62f5a106bfd138459e49770292111a89
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 16:40:59 -0800

    Searches by id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,7 @@
 -- Lists all the cities of California in hbtn_0d_usa database.
--- Uses hbtn_0d_usa database.
-USE `hbtn_0d_usa`;
 -- Lists all the cities of California.
 SELECT `id`, `name` FROM `cities`
 	WHERE `state_id` IN (
 		SELECT `id` FROM `states`
-		WHERE `name` = `California`
-	)
-	ORDER BY `id`;
+		WHERE `id` = 1
+	);
