80bc37bd73fedd2a190bf67f5fc3fbbb7081c498
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 16:09:59 -0800

    Uses different primary key syntax

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,8 +5,9 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 -- Creates a cities table.
 CREATE TABLE IF NOT EXISTS `cities` (
-	`id` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	`id` INT AUTO_INCREMENT UNIQUE NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
 	`state_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
 	FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
 );
