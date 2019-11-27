41b0baa61440cda215d25edea5b488df47a8c4cf
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 16:15:22 -0800

    Removes UNIQUE constraint on city id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 -- Creates a cities table.
 CREATE TABLE IF NOT EXISTS `cities` (
-	`id` INT AUTO_INCREMENT UNIQUE NOT NULL,
+	`id` INT AUTO_INCREMENT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
 	`state_id` INT NOT NULL,
 	PRIMARY KEY (`id`),
