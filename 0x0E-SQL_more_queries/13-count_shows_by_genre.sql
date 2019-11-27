ad9a171563ae62a9267c8346f1e7d2a13b570e91
Marco Chan <inspiredtolive0@gmail.com>
2019-11-26 18:04:54 -0800

    Lists all genres with the number of shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all genres with the number of shows.
 -- Lists all genres with the number of shows.
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 	FROM tv_genres, tv_show_genres
 	WHERE tv_genres.id = tv_show_genres.genre_id
 	GROUP BY genre
