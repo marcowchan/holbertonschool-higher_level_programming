592a47dc2c3a299fce0b38afa8c771a9140d3fbb
Marco Chan <inspiredtolive0@gmail.com>
2020-01-13 23:44:31 -0800

    Handles HTTPErrors

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Handles HTTPErrors."""
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    r = requests.get(argv[1])
+    if r.status_code >= 400:
+        print("Error code: ", r.status_code)
+    else:
+        print(r.text)
