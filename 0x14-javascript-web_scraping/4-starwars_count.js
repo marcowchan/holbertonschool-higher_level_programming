33edd7568c8726fb638c8255e2977bf35a6d5696
Marco Chan <inspiredtolive0@gmail.com>
2020-02-11 14:12:26 -0800

    Correctly handles the error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const wedge = 'https://swapi.co/api/people/18/';
 request(process.argv[2], { json: true }, (err, response, body) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
   console.log(body.results.reduce((prev, curr) => curr.characters.includes(wedge) ? 1 + prev : prev, 0));
 });
