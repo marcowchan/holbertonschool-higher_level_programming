67123ad9043fcd0c2394605e934b2b3e53634371
Marco Chan <inspiredtolive0@gmail.com>
2020-02-11 13:12:32 -0800

    Displays the number of tasks completed per user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const count = {};
+request(process.argv[2], { json: true }, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  body.forEach(({ userId, completed }) => {
+    count[userId] = count[userId] || 0;
+    count[userId] += completed ? 1 : 0;
+  });
+  console.log(count);
+});
