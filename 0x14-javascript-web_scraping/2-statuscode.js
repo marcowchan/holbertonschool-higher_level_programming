82ce9b40a7c032dbd4bc4be1675797d7eaec6036
Marco Chan <inspiredtolive0@gmail.com>
2020-02-11 12:10:21 -0800

    Displays the status code of a response

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('statusCode:', response && response.statusCode);
+});
