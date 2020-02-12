8adb899344e03196db113e56a6f992b402b87eb2
Marco Chan <inspiredtolive0@gmail.com>
2020-02-11 17:18:18 -0800

    Searches through each character in each film

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const wedge = 'https://swapi.co/api/people/18/';
+const wedge = '18';
 request(process.argv[2], { json: true }, (err, response, body) => {
   if (err) {
     return console.log(err);
   }
-  console.log(body.results.reduce((prev, curr) => curr.characters.includes(wedge) ? 1 + prev : prev, 0));
+  console.log(body.results.reduce((prev, curr) => {
+    curr.characters.forEach(char => char.includes(wedge) && prev++);
+    return prev;
+  }, 0));
 });
