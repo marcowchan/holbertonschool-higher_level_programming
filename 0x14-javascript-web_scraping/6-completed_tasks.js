8128f3ffb57b641564c8d89b3847130f6b74b93d
Marco Chan <inspiredtolive0@gmail.com>
2020-02-11 17:43:31 -0800

    Does not include users with 0 completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const count = {};
 request(process.argv[2], { json: true }, (err, response, body) => {
   if (err) {
     return console.log(err);
   }
-  body.forEach(({ userId, completed }) => {
-    count[userId] = count[userId] || 0;
-    count[userId] += completed ? 1 : 0;
-  });
-  console.log(count);
+  console.log(body.reduce((count, { userId, completed }) => {
+    completed && (count[userId] ? count[userId]++ : count[userId] = 1);
+    return count;
+  }, {}));
 });
